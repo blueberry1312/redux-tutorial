13469abab82a330c1d8270d4ffffed3ff333a2aa
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-04-07 22:43:48 -0400

    test redux

---
+++ src/components/CartItem.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid';
+
+import { useDispatch } from 'react-redux';
+import { removeItem, increase, decrease } from '../features/cart/cartSlice';
+
+const CartItem = ({ id, img, title, price, amount }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <article className='cart-item'>
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className='item-price'>${price}</h4>
+        {/* remove button */}
+        <button
+          className='remove-btn'
+          onClick={() => {
+            dispatch(removeItem(id));
+          }}
+        >
+          remove
+        </button>
+      </div>
+      <div>
+        {/* increase amount */}
+        <button
+          className='amount-btn'
+          onClick={() => {
+            dispatch(increase({ id }));
+          }}
+        >
+          <ChevronUpIcon />
+        </button>
+        {/* amount */}
+        <p className='amount'>{amount}</p>
+        {/* decrease amount */}
+        <button
+          className='amount-btn'
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+            }
+            dispatch(decrease({ id }));
+          }}
+        >
+          <ChevronDownIcon />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default CartItem;
